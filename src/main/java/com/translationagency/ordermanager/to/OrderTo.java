3dd4e2753c48ed2335f0756e4b8cd8d839de0a57
Denys Filonenko <filonenko.denys94@gmail.com>
2023-07-31 15:47:04 +0300

    constraints fix, add doc without translator to populate.sql, change field types in entities to store possible null values

--- src/main/java/com/translationagency/ordermanager/to/OrderTo.java
+++ src/main/java/com/translationagency/ordermanager/to/OrderTo.java
@@ -5,6 +5,6 @@ import com.translationagency.ordermanager.entity.OrderStatus;
 import java.time.LocalDate;
 
 public record OrderTo(int id, String formattedId, String customerName, String customerPhone, String customerEmail,
-                      int prepaid, int surcharge, int summaryCost, LocalDate creationDate,
+                      int prepaid, Integer surcharge, Integer summaryCost, LocalDate creationDate,
                       LocalDate deliveryDate, OrderStatus orderStatus, String note) {
 }
