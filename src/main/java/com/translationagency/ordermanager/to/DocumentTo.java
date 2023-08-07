7f14f59d58675e7b3ebdabff893d15423b017574
Denys Filonenko <filonenko.denys94@gmail.com>
2023-08-07 19:26:45 +0300

    add isHardComplexity to DocumentTo, private->public in getTo DocumentUtil

--- src/main/java/com/translationagency/ordermanager/to/DocumentTo.java
+++ src/main/java/com/translationagency/ordermanager/to/DocumentTo.java
@@ -2,7 +2,7 @@ package com.translationagency.ordermanager.to;
 
 import com.translationagency.ordermanager.entity.Language;
 
-public record DocumentTo(int id, int orderId, Language language, int officeRate, double signsNumber,
+public record DocumentTo(int id, int orderId, Language language, boolean isHardComplexity, int officeRate, Double signsNumber,
                          int notarizationCost, int officeCost, String translatorName,
                          String translatorRate, Integer translatorTax) {
 }
--- src/main/java/com/translationagency/ordermanager/util/DocumentUtil.java
+++ src/main/java/com/translationagency/ordermanager/util/DocumentUtil.java
@@ -3,6 +3,7 @@ package com.translationagency.ordermanager.util;
 import com.translationagency.ordermanager.entity.Document;
 import com.translationagency.ordermanager.entity.Language;
 import com.translationagency.ordermanager.entity.LanguageRate;
+import com.translationagency.ordermanager.entity.Translator;
 import com.translationagency.ordermanager.to.DocumentTo;
 
 import java.util.ArrayList;
@@ -19,20 +20,16 @@ public class DocumentUtil {
         return documents.stream().map(DocumentUtil::getTo).toList();
     }
 
-    private static DocumentTo getTo(Document document) {
-        return new DocumentTo(document.getOrder().id(), document.id(), document.getDocumentLanguage(),
+    public static DocumentTo getTo(Document document) {
+        return new DocumentTo(document.getOrder().id(), document.id(), document.getDocumentLanguage(), document.isHardComplexity(),
                 document.getOfficeRate(), document.getSignsNumber(), document.getNotarizationCost(),
                 document.getOfficeCost(), document.getTranslator() != null ? document.getTranslator().getName() : null,
                 document.getTranslatorRate(),
                 document.getTranslatorTax());
     }
 
-    public static String computeTranslatorRate (Document document) {
-        if (document.getTranslatorRate() != null) {
-            return document.getTranslatorRate();
-        }
-
-        LanguageRate translatorRate = getRateByLanguage(document.getTranslator().getRates(),
+    public static String computeTranslatorRate (Document document, Translator translator) {
+        LanguageRate translatorRate = getRateByLanguage(translator.getRates(),
                 document.getDocumentLanguage());
 
         if (document.isHardComplexity()){
@@ -42,14 +39,14 @@ public class DocumentUtil {
         return formatTranslatorRate(translatorRate.getCommonRate(), translatorRate.getSigns());
     }
 
+    private static LanguageRate getRateByLanguage(List<LanguageRate> rates, Language language) {
+        //TODO create reasonable exception
+        return rates.stream().filter(rate -> rate.getLanguage() == language).findAny().orElseThrow(() -> new RuntimeException("Rate not found"));
+    }
+
     private static String formatTranslatorRate(int price, double singsInThousands) {
         String format = "%s/%s";
         return String.format(format, price, (int) (singsInThousands * 1000));
 
-    }
-
-    private static LanguageRate getRateByLanguage(List<LanguageRate> rates, Language language) {
-        //TODO create reasonable exception
-        return rates.stream().filter(rate -> rate.getLanguage() == language).findAny().orElseThrow(() -> new RuntimeException("Rate not found"));
     }
 }
